97d0796a33a45c94e798daea978fa34c688917fd
Ilya Golubkov <golubkov@mcad.ru>
2024-12-28 08:29:39 +0600

    Add: husky precommit

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -10,4 +10,3 @@ app.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }));
 app.listen(5000, () => {
   console.log('Server started on port 5000');
 });
-
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -9,6 +9,4 @@ const container = document.getElementById('root');
 if (!container) throw new Error('Root element not found');
 const root = createRoot(container);
 
-root.render(
-  <App />,
-);
+root.render(<App />);
